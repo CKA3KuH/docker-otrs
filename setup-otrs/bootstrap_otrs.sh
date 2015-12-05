4e79071754e4f05065e97076571afcf22a964b51
CKA3KuH <CKA3KuH@gmail.com>
2015-12-05 14:03:45 +0200

    Update bootstrap_otrs.sh

--- setup-otrs/bootstrap_otrs.sh
+++ setup-otrs/bootstrap_otrs.sh
@@ -4,7 +4,7 @@ set -e
 /usr/bin/mysql_install_db --datadir=/var/lib/mysql --user=mysql
 # Workaround for some versions of /etc/init.d/mysqld
 touch /etc/sysconfig/network
-rpm -ivh http://ftp.otrs.org/pub/otrs/RPMS/rhel/6/otrs-3.2.5-01.noarch.rpm 
+rpm -ivh http://ftp.otrs.org/pub/otrs/RPMS/rhel/6/otrs-5.0.4-01.noarch.rpm 
 /etc/init.d/mysqld start
 mysql -u root mysql < /opt/setup-otrs/create_otrs_db.sql
 mysql -u root otrs < /opt/setup-otrs/otrs_tables.sql
@@ -12,11 +12,11 @@ mysql -u root otrs < /opt/setup-otrs/otrs_tables.sql
       DatabaseUser=otrs DatabasePw=otrs DatabaseHost=127.0.0.1 SecureMode=1
 /opt/otrs/bin/otrs.SetPassword.pl root@localhost root
 /opt/otrs/bin/otrs.PackageManager.pl -a install \
-       -p http://ftp.otrs.org/pub/otrs/packages/:FAQ-2.2.3.opm
+       -p http://ftp.otrs.org/pub/otrs/packages/:FAQ-5.0.2.opm
 /opt/otrs/bin/otrs.PackageManager.pl -a install \
-       -p http://ftp.otrs.org/pub/otrs/packages/:Support-1.4.5.opm
+       -p http://ftp.otrs.org/pub/otrs/packages/:Support-1.5.4.opm
 /opt/otrs/bin/otrs.PackageManager.pl -a install \
-       -p http://ftp.otrs.org/pub/otrs/itsm/bundle32/:ITSM-3.2.3.opm
+       -p http://ftp.otrs.org/pub/otrs/itsm/bundle32/:ITSM-3.2.9.opm
 /etc/init.d/mysqld stop
 chown -R otrs:apache /opt/otrs
 chmod -R g+rwX /opt/otrs
